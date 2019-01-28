955a827456122fe40c3c143654aa77a62a5416ad
Elias Aebi <eyelash@users.noreply.github.com>
2019-01-28 20:04:45 +0100

    parse variables and scopes

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
 #include "parser.hpp"
+#include <vector>
+#include <fstream>
 #include <iostream>
 
+std::vector<char> read_file(const char* file_name) {
+	std::ifstream file(file_name);
+	return std::vector<char>(std::istreambuf_iterator<char>(file), std::istreambuf_iterator<char>());
+}
+
 int main(int argc, char** argv) {
-	Parser parser(argv[1]);
+	std::vector<char> file = read_file(argv[1]);
+	Parser parser(file.data(), file.size());
 	Expression* expr = parser.parse();
 	std::int32_t value;
 	expr->evaluate(&value);
--- parser.hpp
+++ parser.hpp
@@ -1,5 +1,6 @@
 #include "ast.hpp"
 #include <cstddef>
+#include <map>
 
 class StringView {
 	const char* string;
@@ -72,13 +73,43 @@ static constexpr BinaryOperator operators[][4] = {
 	}
 };
 
+class Scope {
+	Scope* parent;
+	std::map<StringView, Expression*> variables;
+public:
+	Scope(Scope* parent = nullptr): parent(parent) {}
+	Scope* get_parent() const {
+		return parent;
+	}
+	void add_variable(const StringView& name, Expression* value) {
+		variables[name] = value;
+	}
+	Expression* look_up(const StringView& name) const {
+		auto iterator = variables.find(name);
+		if (iterator != variables.end()) {
+			return iterator->second;
+		}
+		if (parent) {
+			return parent->look_up(name);
+		}
+		return nullptr;
+	}
+};
+
 class Parser {
 	StringView string;
+	Scope* current_scope;
 	static constexpr bool white_space(char c) {
-		return c == ' ' || c == '\n' || c == '\t';
+		return c == ' ' || c == '\t' || c == '\n' || c == '\r';
 	}
 	static constexpr bool numeric(char c) {
 		return c >= '0' && c <= '9';
+	}
+	static constexpr bool alphabetic(char c) {
+		return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c == '_';
+	}
+	static constexpr bool alphanumeric(char c) {
+		return alphabetic(c) || numeric(c);
 	}
 	template <class F> void parse_all(F f) {
 		std::size_t i = 0;
@@ -110,6 +141,23 @@ class Parser {
 		}
 		return new Number(number);
 	}
+	StringView parse_identifier() {
+		std::size_t i = 0;
+		if (i < string.size() && alphabetic(string[i])) {
+			++i;
+			while (i < string.size() && alphanumeric(string[i])) {
+				++i;
+			}
+			StringView result = string.substr(0, i);
+			string = string.substr(i);
+			return result;
+		}
+		return StringView();
+	}
+	Expression* parse_variable() {
+		StringView identifier = parse_identifier();
+		return current_scope->look_up(identifier);
+	}
 	BinaryOperator parse_operator(int level) {
 		for (int i = 0; operators[level][i]; ++i) {
 			if (parse(operators[level][i].string)) {
@@ -119,6 +167,13 @@ class Parser {
 		return BinaryOperator();
 	}
 	Expression* parse_expression_last() {
+		if (parse("{")) {
+			parse_all(white_space);
+			Expression* expression = parse_scope();
+			parse_all(white_space);
+			parse("}");
+			return expression;
+		}
 		if (parse("(")) {
 			parse_all(white_space);
 			Expression* expression = parse_expression();
@@ -126,7 +181,10 @@ class Parser {
 			parse(")");
 			return expression;
 		}
-		return parse_number();
+		if (0 < string.size() && numeric(string[0])) {
+			return parse_number();
+		}
+		return parse_variable();
 	}
 	Expression* parse_expression(int level = 0) {
 		if (level == 2) {
@@ -142,10 +200,36 @@ class Parser {
 		}
 		return left;
 	}
+	Expression* parse_scope() {
+		Scope scope(current_scope);
+		current_scope = &scope;
+		Expression* result = nullptr;
+		while (string.size() > 0) {
+			if (parse("let")) {
+				parse_all(white_space);
+				StringView name = parse_identifier();
+				parse_all(white_space);
+				parse("=");
+				parse_all(white_space);
+				Expression* expression = parse_expression();
+				scope.add_variable(name, expression);
+			}
+			else if (parse("return")) {
+				parse_all(white_space);
+				result = parse_expression();
+			}
+			else {
+				break;
+			}
+		}
+		current_scope = scope.get_parent();
+		return result;
+	}
 public:
-	Parser(const char* string): string(string) {}
+	Parser(const char* string): string(string), current_scope(nullptr) {}
+	Parser(const char* string, std::size_t length): string(string, length), current_scope(nullptr) {}
 	Expression* parse() {
 		parse_all(white_space);
-		return parse_expression();
+		return parse_scope();
 	}
 };
