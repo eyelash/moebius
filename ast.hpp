9b0ad7a688cdf29e5f403024a853fa67ec9475a2
Elias Aebi <eyelash@users.noreply.github.com>
2019-03-03 14:28:54 +0100

    closures

--- ast.hpp
+++ ast.hpp
@@ -30,6 +30,21 @@ public:
 
 class Value;
 
+class Callable {
+public:
+	virtual Value* call(const std::vector<Value*>& arguments) = 0;
+};
+
+class Value {
+public:
+	virtual std::int32_t get_int() {
+		return 0;
+	}
+	virtual Callable* get_callable() {
+		return nullptr;
+	}
+};
+
 class Expression {
 public:
 	using Environment = Table<Value>;
@@ -39,25 +54,13 @@ public:
 	}
 };
 
-class Function;
-
-class Value: public Expression {
-public:
-	Value* evaluate(const Environment& environment) override {
-		return this;
-	}
-	virtual std::int32_t get_int() {
-		return 0;
-	}
-	virtual Function* get_function() {
-		return nullptr;
-	}
-};
-
-class Number: public Value {
+class Number: public Expression, public Value {
 	std::int32_t value;
 public:
 	Number(std::int32_t value): value(value) {}
+	Value* evaluate(const Environment& environment) override {
+		return new Number(value);
+	}
 	std::int32_t get_int() override {
 		return value;
 	}
@@ -128,20 +131,29 @@ public:
 	}
 };
 
-class Function: public Value {
+class Function: public Expression {
 	Expression* expression;
 	std::vector<StringView> argument_names;
 public:
 	Function(Expression* expression, const std::vector<StringView>& argument_names): expression(expression), argument_names(argument_names) {}
-	Function* get_function() override {
-		return this;
-	}
-	Value* call(const std::vector<Value*>& arguments) {
-		Table<Value> table;
-		for (std::size_t i = 0; i < argument_names.size() && i < arguments.size(); ++i) {
-			table.insert(argument_names[i], arguments[i]);
-		}
-		return expression->evaluate(table);
+	Value* evaluate(const Environment& environment) override {
+		class Closure: public Value, public Callable {
+			Function* function;
+			Environment environment;
+		public:
+			Closure(Function* function, const Environment& environment): function(function), environment(environment) {}
+			Callable* get_callable() override {
+				return this;
+			}
+			Value* call(const std::vector<Value*>& arguments) override {
+				const std::vector<StringView>& argument_names = function->argument_names;
+				for (std::size_t i = 0; i < argument_names.size() && i < arguments.size(); ++i) {
+					environment.insert(argument_names[i], arguments[i]);
+				}
+				return function->expression->evaluate(environment);
+			}
+		};
+		return new Closure(this, environment);
 	}
 };
 
@@ -160,11 +172,11 @@ class Call: public Expression {
 public:
 	Call(Expression* expression, const std::vector<Expression*>& arguments): expression(expression), arguments(arguments) {}
 	Value* evaluate(const Environment& environment) override {
-		Function* function = expression->evaluate(environment)->get_function();
+		Callable* callable = expression->evaluate(environment)->get_callable();
 		std::vector<Value*> argument_values;
 		for (Expression* argument: arguments) {
 			argument_values.push_back(argument->evaluate(environment));
 		}
-		return function->call(argument_values);
+		return callable->call(argument_values);
 	}
 };
