c6692c5a7d34690fe83f582190fa576c122cac9d
Elias Aebi <eyelash@users.noreply.github.com>
2019-04-06 09:54:49 +0200

    prepare

--- codegen.hpp
+++ codegen.hpp
@@ -25,10 +25,10 @@ public:
 
 using Environment = Table<Value>;
 
-class NumberValue: public Value {
+class CompiletimeNumber: public Value {
 	std::int32_t value;
 public:
-	NumberValue(std::int32_t value): value(value) {}
+	CompiletimeNumber(std::int32_t value): value(value) {}
 	std::int32_t get_int() override {
 		return value;
 	}
@@ -59,66 +59,67 @@ public:
 	}
 };
 
+class CodegenVisitor: public Visitor {
+	Value* value;
+	const Environment& environment;
+public:
+	CodegenVisitor(const Environment& environment): value(nullptr), environment(environment) {}
+	Value* evaluate(const Expression* expression) {
+		value = nullptr;
+		expression->accept(this);
+		return value;
+	}
+	void visit_number(const Number* number) override {
+		value = new CompiletimeNumber(number->get_value());
+	}
+	void visit_addition(const Addition* addition) override {
+		Value* left = evaluate(addition->get_left());
+		Value* right = evaluate(addition->get_right());
+		value = new CompiletimeNumber(left->get_int() + right->get_int());
+	}
+	void visit_subtraction(const Subtraction* subtraction) override {
+		Value* left = evaluate(subtraction->get_left());
+		Value* right = evaluate(subtraction->get_right());
+		value = new CompiletimeNumber(left->get_int() - right->get_int());
+	}
+	void visit_multiplication(const Multiplication* multiplication) override {
+		Value* left = evaluate(multiplication->get_left());
+		Value* right = evaluate(multiplication->get_right());
+		value = new CompiletimeNumber(left->get_int() * right->get_int());
+	}
+	void visit_division(const Division* division) override {
+		Value* left = evaluate(division->get_left());
+		Value* right = evaluate(division->get_right());
+		value = new CompiletimeNumber(left->get_int() / right->get_int());
+	}
+	void visit_remainder(const Remainder* remainder) override {
+		Value* left = evaluate(remainder->get_left());
+		Value* right = evaluate(remainder->get_right());
+		value = new CompiletimeNumber(left->get_int() % right->get_int());
+	}
+	void visit_function(const Function* function) override {
+		value = new Closure(function, environment);
+	}
+	void visit_argument(const Argument* argument) override {
+		value = environment.look_up(argument->get_name());
+	}
+	void visit_call(const Call* call) override {
+		Closure* closure = evaluate(call->get_expression())->get_closure();
+		std::vector<Value*> argument_values;
+		for (const Expression* argument: call->get_arguments()) {
+			argument_values.push_back(evaluate(argument));
+		}
+		const std::vector<StringView>& argument_names = closure->get_argument_names();
+		Environment new_environment(closure->get_environment());
+		for (std::size_t i = 0; i < argument_names.size() && i < argument_values.size(); ++i) {
+			new_environment.insert(argument_names[i], argument_values[i]);
+		}
+		CodegenVisitor visitor(new_environment);
+		value = visitor.evaluate(closure->get_expression());
+	}
+};
+
 Value* evaluate(const Expression* expression) {
-	class CodegenVisitor: public Visitor {
-		Value* value;
-		const Environment& environment;
-	public:
-		CodegenVisitor(const Environment& environment): value(nullptr), environment(environment) {}
-		Value* evaluate(const Expression* expression) {
-			value = nullptr;
-			expression->accept(this);
-			return value;
-		}
-		void visit_number(const Number* number) override {
-			value = new NumberValue(number->get_value());
-		}
-		void visit_addition(const Addition* addition) override {
-			Value* left = evaluate(addition->get_left());
-			Value* right = evaluate(addition->get_right());
-			value = new NumberValue(left->get_int() + right->get_int());
-		}
-		void visit_subtraction(const Subtraction* subtraction) override {
-			Value* left = evaluate(subtraction->get_left());
-			Value* right = evaluate(subtraction->get_right());
-			value = new NumberValue(left->get_int() - right->get_int());
-		}
-		void visit_multiplication(const Multiplication* multiplication) override {
-			Value* left = evaluate(multiplication->get_left());
-			Value* right = evaluate(multiplication->get_right());
-			value = new NumberValue(left->get_int() * right->get_int());
-		}
-		void visit_division(const Division* division) override {
-			Value* left = evaluate(division->get_left());
-			Value* right = evaluate(division->get_right());
-			value = new NumberValue(left->get_int() / right->get_int());
-		}
-		void visit_remainder(const Remainder* remainder) override {
-			Value* left = evaluate(remainder->get_left());
-			Value* right = evaluate(remainder->get_right());
-			value = new NumberValue(left->get_int() % right->get_int());
-		}
-		void visit_function(const Function* function) override {
-			value = new Closure(function, environment);
-		}
-		void visit_argument(const Argument* argument) override {
-			value = environment.look_up(argument->get_name());
-		}
-		void visit_call(const Call* call) override {
-			Closure* closure = evaluate(call->get_expression())->get_closure();
-			std::vector<Value*> argument_values;
-			for (const Expression* argument: call->get_arguments()) {
-				argument_values.push_back(evaluate(argument));
-			}
-			const std::vector<StringView>& argument_names = closure->get_argument_names();
-			Environment new_environment(closure->get_environment());
-			for (std::size_t i = 0; i < argument_names.size() && i < argument_values.size(); ++i) {
-				new_environment.insert(argument_names[i], argument_values[i]);
-			}
-			CodegenVisitor visitor(new_environment);
-			value = visitor.evaluate(closure->get_expression());
-		}
-	};
 	Environment environment;
 	CodegenVisitor visitor(environment);
 	return visitor.evaluate(expression);
